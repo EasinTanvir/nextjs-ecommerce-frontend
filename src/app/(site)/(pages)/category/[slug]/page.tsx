6da9b2f253e6b969fe2726ff15cfa91b100ea938
EasinTanvir <easintanvir102@gmail.com>
2026-05-23 12:00:25 +0600

    update site bar

--- src/app/(site)/(pages)/category/[slug]/page.tsx
+++ src/app/(site)/(pages)/category/[slug]/page.tsx
@@ -1,13 +1,8 @@
 import ShopWithSidebar from "@/components/ShopWithSidebar";
 
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Shop Page | NextCommerce Nextjs E-commerce template",
-  description: "This is Shop Page for NextCommerce Template",
-  // other metadata
-};
+const ShopWithSidebarPage = async ({ params }) => {
+  const { slug } = await params;
 
-const ShopWithSidebarPage = () => {
   return (
     <main>
       <ShopWithSidebar />
