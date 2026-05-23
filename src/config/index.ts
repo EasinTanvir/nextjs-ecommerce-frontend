4601eca28e5be4ce7703e4ec2acaf75d5c9c20c8
EasinTanvir <easintanvir102@gmail.com>
2026-05-23 11:52:38 +0600

    added category page

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,7 @@
-const BaseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://ecommerce.test/api";
+const BaseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  process.env.BASE_URL ||
+  "http://ecommerce.test/api";
 const CACHING_TIME_REACT_QUERY = 60 * 1000;
 
 export { BaseUrl, CACHING_TIME_REACT_QUERY };
