e9b867682c320e7e335b164ed931c07592b24932
EasinTanvir <easintanvir102@gmail.com>
2026-05-23 23:05:25 +0600

    added payment method

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -7,42 +7,19 @@ import ShippingMethod from "./ShippingMethod";
 import PaymentMethod from "./PaymentMethod";
 import Coupon from "./Coupon";
 import Billing from "./Billing";
+import toast from "react-hot-toast";
 
 const Checkout = () => {
   return (
     <>
-      <Breadcrumb title={"Checkout"} pages={["checkout"]} />
-      <section className="overflow-hidden py-20 bg-gray-2">
+      <section className="overflow-hidden py-32 bg-gray-2">
         <div className="max-w-[1170px] w-full mx-auto px-4 sm:px-8 xl:px-0">
           <form>
             <div className="flex flex-col lg:flex-row gap-7.5 xl:gap-11">
               {/* <!-- checkout left --> */}
               <div className="lg:max-w-[670px] w-full">
-                {/* <!-- login box --> */}
-                <Login />
-
                 {/* <!-- billing details --> */}
                 <Billing />
-
-                {/* <!-- address box two --> */}
-                <Shipping />
-
-                {/* <!-- others note box --> */}
-                <div className="bg-white shadow-1 rounded-[10px] p-4 sm:p-8.5 mt-7.5">
-                  <div>
-                    <label htmlFor="notes" className="block mb-2.5">
-                      Other Notes (optional)
-                    </label>
-
-                    <textarea
-                      name="notes"
-                      id="notes"
-                      rows={5}
-                      placeholder="Notes about your order, e.g. speacial notes for delivery."
-                      className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full p-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-                    ></textarea>
-                  </div>
-                </div>
               </div>
 
               {/* // <!-- checkout right --> */}
@@ -123,20 +100,21 @@ const Checkout = () => {
                 </div>
 
                 {/* <!-- coupon box --> */}
-                <Coupon />
+                {/* <Coupon /> */}
 
                 {/* <!-- shipping box --> */}
-                <ShippingMethod />
+                {/* <ShippingMethod /> */}
 
                 {/* <!-- payment box --> */}
                 <PaymentMethod />
 
                 {/* <!-- checkout button --> */}
                 <button
-                  type="submit"
+                  type="button"
+                  onClick={() => toast.error("Invalid stripe key")}
                   className="w-full flex justify-center font-medium text-white bg-blue py-3 px-6 rounded-md ease-out duration-200 hover:bg-blue-dark mt-7.5"
                 >
-                  Process to Checkout
+                  Pay Now
                 </button>
               </div>
             </div>
